cc83a708eed2830e339056b75389e0787b6ab547
Craig Buchanan <craig.buchanan@cogniza.com>
2024-10-18 14:30:42 -0500

    adding mode adding autorename adding mute adding strictconflict adding selectuser adding rootfolderid

--- PsDropbox/Public/Send-DropboxFile.ps1
+++ PsDropbox/Public/Send-DropboxFile.ps1
@@ -4,37 +4,61 @@ https://www.dropbox.com/developers/documentation/http/documentation#files-upload
 #>
 function Send-DropboxFile {
 
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess)]
     param (
         [Parameter(Mandatory)]
-        [string]$ApiKey,
+        [string]$AccessToken,
 
         [Parameter(Mandatory)]
         [string]$Source,
 
         [Parameter(Mandatory)]
-        [string]$Destination
+        [string]$Destination,
+
+        [Parameter()]
+        [ValidateSet('add','overwrite','update')]
+        [string]$Mode = 'add',
+
+        [Parameter()]
+        [switch]$AutoRename,
+
+        [Parameter()]
+        [switch]$Mute,
+
+        [Parameter()]
+        [switch]$StrictConflict,
+
+        [string]$SelectUser,
+        [string]$RootFolderId
     )
 
     $Headers = @{
-        Authorization = 'Bearer ' + $ApiKey
-        'Dropbox-API-Arg' = @{path = $Destination} | ConvertTo-Json -Compress
+        Authorization = 'Bearer ' + $AccessToken
+        'Dropbox-API-Arg' = @{
+            path = $Destination
+            mode = $Mode
+            autorename = [bool]$AutoRename
+            mute = [bool]$Mute
+            strict_conflict = [bool]$StrictConflict
+        } | ConvertTo-Json -Compress
     }
+
+    if ($SelectUser) {$Headers['Dropbox-API-Select-User']=$SelectUser}
+    if ($RootFolderId) {$Headers['Dropbox-API-Path-Root']=@{'.tag'='root'; root=$RootFolderId} | ConvertTo-Json -Compress}
+
     Write-Debug $Headers
 
     $Uri = 'https://content.dropboxapi.com/2/files/upload'
     Write-Debug "Uri: $Uri"
 
-    $Response = Invoke-WebRequest -Uri $Uri -Headers $Headers -Method Post -ContentType 'application/octet-stream' -InFile $Source
+    if ($PSCmdlet.ShouldProcess("$Source --> $Destination", "Send-DropboxFile")) {
 
-    if ($Response.Content) {
+        $Response = Invoke-WebRequest -Uri $Uri -Headers $Headers -Method Post -ContentType 'application/octet-stream' -InFile $Source
 
-        # convert JSON to Hashtable
-        $Content = $Response.Content | ConvertFrom-Json
-
-        # return data
-        $Content
-
+        if ($Response.Content) {
+            $Response.Content | ConvertFrom-Json
+        }
+    
     }
 
 }
--- Tests/Public/Send-DropboxFile.Tests.ps1
+++ Tests/Public/Send-DropboxFile.Tests.ps1
@@ -12,23 +12,55 @@ BeforeAll {
 
 Describe 'Send-DropboxFile' {
 
-    BeforeEach {
+    Context "Parameter validation" {
 
-        # arrange
-        $ApiKey = '2134d8d5-d1b4-4a1d-89ac-f44a96514bb5'
-        $Source = "~/Prime_Numbers.txt"
-        $Destination = "/Homework/math/Prime_Numbers.txt"
-
-    }
+        BeforeAll {
+          $Command = Get-Command "Send-DropboxFile"
+        }
+    
+        $Parameters = @(
+            @{Name='AccessToken';Type='string';Mandatory=$true}
+            @{Name='Source';Type='string';Mandatory=$true}
+            @{Name='Destination';Type='string';Mandatory=$true}
+            @{Name='Mode';Type='string';Mandatory=$false}
+            @{Name='AutoRename';Type='switch';Mandatory=$false}
+            @{Name='Mute';Type='switch';Mandatory=$false}
+            @{Name='StrictConflict';Type='switch';Mandatory=$false}
+            @{Name='SelectUser';Type='string';Mandatory=$false}
+            @{Name='RootFolderId';Type='string';Mandatory=$false}
+        )
+    
+        Context 'Type' {
+            it '<Name> is a <Type>' -TestCases $Parameters {
+                param($Name, $Type, $Mandatory)
+              
+                $Command | Should -HaveParameter $Name -Type $type
+            }    
+        }
+    
+        Context 'Mandatory' {
+            it '<Name> mandatory is <Mandatory>' -TestCases $Parameters {
+                param($Name, $Type, $Mandatory)
+              
+                if ($Mandatory) { $Command | Should -HaveParameter $Name -Mandatory }
+                else { $Command | Should -HaveParameter $Name -Not -Mandatory }
+            }    
+        }
+        
+    } # /context
 
     Context "Request" {
 
         BeforeEach {
+            # arrange
+            $AccessToken = '2134d8d5-d1b4-4a1d-89ac-f44a96514bb5'
+            $Source = "~/Prime_Numbers.txt"
+            $Destination = "/Homework/math/Prime_Numbers.txt"
 
             Mock Invoke-WebRequest {}
 
             # act
-            Send-DropboxFile -ApiKey $ApiKey -Source $Source -Destination $Destination
+            Send-DropboxFile -AccessTo $AccessToken -Source $Source -Destination $Destination
 
         }
 
@@ -50,7 +82,7 @@ Describe 'Send-DropboxFile' {
         It "uses the correct Authorization header" {
             # assert
             Should -Invoke -CommandName Invoke-WebRequest -ParameterFilter {
-                $Headers.Authorization -eq "Bearer $ApiKey"
+                $Headers.Authorization -eq "Bearer $AccessToken"
             }
         }
 
@@ -64,6 +96,42 @@ Describe 'Send-DropboxFile' {
         It "uses the correct InFile" {
             # assert
             Should -Invoke -CommandName Invoke-WebRequest -ParameterFilter { $InFile -eq $Source }
+        }
+
+        Context 'when the SelectUser parameter is supplied' {
+
+            BeforeEach {
+                # arrange
+                Mock Invoke-WebRequest {}
+                # act
+                Send-DropboxFile -AccessTo $AccessToken -Source $Source -Destination $Destination -SelectUser 'SelectUser'
+            }
+
+            It "adds the Dropbox-API-Select-User header" {
+                # assert
+                Should -Invoke -CommandName Invoke-WebRequest -ParameterFilter {
+                    $Headers.'Dropbox-API-Select-User' -eq 'SelectUser'
+                }
+            }
+
+        }
+
+        Context 'when the RootFolderId parameter is supplied' {
+
+            BeforeEach {
+                # arrange
+                Mock Invoke-WebRequest {}
+                # act
+                Send-DropboxFile -AccessTo $AccessToken -Source $Source -Destination $Destination -RootFolderId 'RootFolderId'
+            }
+
+            It "adds the Dropbox-API-Path-Root header" {
+                # assert
+                Should -Invoke -CommandName Invoke-WebRequest -ParameterFilter {
+                    $Headers.'Dropbox-API-Path-Root' -eq @{root = 'RootFolderId'} | ConvertTo-Json -Compress
+                }
+            }
+
         }
 
     }
